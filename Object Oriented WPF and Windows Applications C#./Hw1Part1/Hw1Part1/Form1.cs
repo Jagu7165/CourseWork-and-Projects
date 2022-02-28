36cf606e1d177069d45af05ee76d02b752f9b682
Jorge Aguinaga <54590716+Jagu7165@users.noreply.github.com>
2022-02-28 14:45:36 -0700

    Add files via upload

---
+++ Object Oriented WPF and Windows Applications C#./Hw1Part1/Hw1Part1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hw1Part1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult myResult;
+            myResult = MessageBox.Show("You typed: " + textBox1.Text,"Caption 1", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            label1.Text = "You clicked the " + myResult.ToString() + " button";
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult myResult;
+            myResult = MessageBox.Show("You typed: " + textBox2.Text, "Caption 1", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+            label2.Text = "You clicked the " + myResult.ToString() + " button";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult myResult;
+            myResult = MessageBox.Show("You typed: " + textBox3.Text, "Caption 1", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Information);
+            label3.Text = "You clicked the " + myResult.ToString() + " button";
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
